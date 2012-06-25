2f678ccd9c4777b05d789da5491e65ab903f8c6e
Aleksandr Skobelev <al.skobelev@gmail.com>
2012-06-25 11:31:59 +0400

    Initial Commit

---
+++ AudioManagerTestTests/AudioManagerTestTests.h
@@ -0,0 +1,13 @@
+//
+//  AudioManagerTestTests.h
+//  AudioManagerTestTests
+//
+//  Created by Alexander Skobelev on 25/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AudioManagerTestTests : SenTestCase
+
+@end
---
+++ AudioManagerTestTests/AudioManagerTestTests.m
@@ -0,0 +1,32 @@
+//
+//  AudioManagerTestTests.m
+//  AudioManagerTestTests
+//
+//  Created by Alexander Skobelev on 25/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AudioManagerTestTests.h"
+
+@implementation AudioManagerTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AudioManagerTestTests");
+}
+
+@end
